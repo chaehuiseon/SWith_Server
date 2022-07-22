4c9dc75388af1fe7ad2d88ac97aa989c83b2c0d3
ympark99 <ympark1230@naver.com>
2022-07-21 17:12:37 -0700

    mockmvc 오류 처리 완료, 환경변수 충돌 해결 필요

--- Swith-Server/src/test/java/com/example/demo/common/BaseTest.java
+++ Swith-Server/src/test/java/com/example/demo/common/BaseTest.java
@@ -2,12 +2,13 @@ package com.example.demo.common;
 
 import com.example.demo.src.entity.Interest;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hibernate.annotations.DynamicInsert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MockMvc;
 
 public class BaseTest {
-    @Autowired
-    public MockMvc mockMvc;
+//    @Autowired
+//    public MockMvc mockMvc;
 
     @Autowired
     public ObjectMapper objectMapper;
@@ -15,7 +16,9 @@ public class BaseTest {
 
     public String email = "test1@naver.com";
     public String password = "password";
-    public Interest interest1 = new Interest(1, "test");
-    public Interest interest2 = new Interest(2, "test2");
+    public Interest interest1 = Interest.builder().interestIdx(0).interestContent("test1").build();
+    public Interest interest2 = Interest.builder().interestIdx(2).interestContent("test2").build();
+//    public Interest interest1 = new Interest(1, "test");
+//    public Interest interest2 = new Interest(2, "test2");
     public String introduction = "안녕하세요, 신입 회원입니다!";
 }
--- Swith-Server/src/test/java/com/example/demo/userControllerTest/SignUpTest.java
+++ Swith-Server/src/test/java/com/example/demo/userControllerTest/SignUpTest.java
@@ -2,22 +2,32 @@ package com.example.demo.userControllerTest;
 
 import com.example.demo.common.BaseTest;
 import com.example.demo.src.dto.request.SignUpRequestDto;
-import com.example.demo.src.entity.Interest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class SignUpTest extends BaseTest {
+    @Autowired
+    public MockMvc mockMvc;
+
+
     @Test
     @DisplayName("회원 가입 테스트(성공)")
     public void signUpTestSuccess() throws Exception {
-
         SignUpRequestDto signUpRequestDto = new SignUpRequestDto(email, password, interest1, interest2, introduction);
 
         mockMvc.perform(post("/v1/signUp")

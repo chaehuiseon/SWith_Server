da8b680ce986835f813ae8b39c23cb9d07f3e8dd
chaehuiseon <smilechs34@gmail.com>
2022-08-05 00:12:15 +0900

    PostGrouopInfo Swagger ApiModelProperty 적용

--- Swith-Server/src/main/java/com/example/demo/src/dto/PostGroupInfoRes.java
+++ Swith-Server/src/main/java/com/example/demo/src/dto/PostGroupInfoRes.java
@@ -1,6 +1,7 @@
 package com.example.demo.src.dto;
 
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,5 +10,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class PostGroupInfoRes {
+    @ApiModelProperty(notes = "스터디 개설한 유저의 PK",example = "1")
     private Long groupIdx;
 }

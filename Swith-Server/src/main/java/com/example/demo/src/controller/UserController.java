878db6608a77eb7c8e2fb251486671de159de035
ympark99 <ympark1230@naver.com>
2022-08-16 15:06:52 +0900

    ready for merge

--- Swith-Server/src/main/java/com/example/demo/src/controller/UserController.java
+++ Swith-Server/src/main/java/com/example/demo/src/controller/UserController.java
@@ -50,17 +50,13 @@ public class UserController {
 //        return LocalDateTime.now().toString();
 //    }
 
-    @GetMapping("/oauth/loginInfo")
+    @ApiOperation("로그인 정보")
+    @GetMapping("/loginInfo")
     @ResponseBody
     public String oauthLoginInfo(Authentication authentication, @AuthenticationPrincipal OAuth2User oAuth2UserPrincipal){
         OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
         Map<String, Object> attributes = oAuth2User.getAttributes();
         System.out.println(attributes);
-        // PrincipalOauth2UserService의 getAttributes내용과 같음
-
-        Map<String, Object> attributes1 = oAuth2UserPrincipal.getAttributes();
-        // attributes == attributes1
-
         return attributes.toString();     //세션에 담긴 user가져올 수 있음음
     }
 

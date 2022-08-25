6438c833433ef2b0cb4357300ce608468b8712df
chaehuiseon <smilechs34@gmail.com>
2022-08-25 12:06:40 +0900

    group create 생성시 Register에adminIdx 등록, 지원서 list adminIdx는 안보이게

--- Swith-Server/src/main/java/com/example/demo/src/repository/ApplicationRepository.java
+++ Swith-Server/src/main/java/com/example/demo/src/repository/ApplicationRepository.java
@@ -23,8 +23,8 @@ public interface ApplicationRepository extends JpaRepository<Application,Long> {
     @Query("select a "+
             "from Application a "+
             "join fetch a.user u "+
-            "where a.groupInfo.groupIdx = :groupIdx and a.status = :status")
-    List<Application> getApplicationListBy(@Param("groupIdx")Long groupIdx, @Param("status")Integer status);
+            "where a.groupInfo.groupIdx = :groupIdx and a.status = :status and a.groupInfo.user.userIdx <> : userIdx")
+    List<Application> getApplicationListBy(@Param("groupIdx")Long groupIdx, @Param("status")Integer status,@Param("userIdx")Long userIdx);
 
 
 
